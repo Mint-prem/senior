2527536919e86e5a0577617902cf55606ce521c0
Mint <mint29006@gmail.com>
2021-06-18 13:59:35 +0700

    ADD method getAllParturition() in parturition controller

--- src/controller/parturition.js
+++ src/controller/parturition.js
@@ -2,4 +2,18 @@ const pool = require(`../database/pool`);
 
 const parturition = {}
 
+parturition.getAllParturition = async()=>{
+    let ret = {}
+        ret.message = "Cannot get data!!"
+    try {
+        const result = await pool.query(`SELECT * FROM parturition`);
+        ret.message = "Sussess :)"
+        console.log(result);
+        console.log(ret.message);
+        return result;
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
 module.exports = parturition;
