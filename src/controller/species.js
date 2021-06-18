be7036ae264420db11e45790922027a686eb00b9
Mint <mint29006@gmail.com>
2021-06-18 13:35:04 +0700

    ADD method getAllSpecies()

--- src/controller/species.js
+++ src/controller/species.js
@@ -2,5 +2,18 @@ const pool = require(`../database/pool`);
 
 const species = {}
 
+species.getAllSpecies=async()=>{
+    let ret = {}
+        ret.message = "Cannot get species"
+    try {
+        const result = await pool.query(`SELECT * FROM species`);
+        ret.message = "Sussess :)"
+        console.log(result);
+        console.log(ret.message);
+        return result;
+    } catch (err) {
+        console.error(err.message);
+    }
+}
 
 module.exports = species;
