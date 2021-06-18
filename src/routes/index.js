6f88f7add4b80e4b30486120b86564052ce93f14
Mint <mint29006@gmail.com>
2021-06-18 14:02:14 +0700

    ADD path /milks in routes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,36 @@
 const express = require("express");
-const cows = require("../controller/cows")
+const cows = require("../controller/cows");
+const milk = require("../controller/milk");
 const species = require("../controller/species");
 const router = express.Router();
 
+//route cow
 router.get('/cows', async(req, res, next) =>{
     const ret = await cows.getAllCow();
     res.send(ret);
 });  
 
+//route species
 router.get('/species', async(req, res, next) =>{
     const ret = await species.getAllSpecies();
     res.send(ret);
 })
+
+//route milk
+router.get('/milks', async(req, res, next) =>{
+    const ret = await milk.getAllMilk();
+    res.send(ret);
+})
+
+//route typecow
+
+//route vaccine
+
+//route vaccine_shedule
+
+//route abdominal
+
+//route parturition
+
 
 module.exports = router;
