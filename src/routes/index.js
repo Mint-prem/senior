caf04e4a29ec4e8f29acc0768f4377bee6949d9e
Mint <mint29006@gmail.com>
2021-06-18 13:40:59 +0700

    ADD path /species in routes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
-const cows = require("../controller/cows")
 const express = require("express");
+const cows = require("../controller/cows")
+const species = require("../controller/species");
 const router = express.Router();
 
 router.get('/cows', async(req, res, next) =>{
     const ret = await cows.getAllCow();
     res.send(ret);
 });  
+
+router.get('/species', async(req, res, next) =>{
+    const ret = await species.getAllSpecies();
+    res.send(ret);
+})
 
 module.exports = router;
