a3a824d7239980e71cb6e7ccf755bf88d847f1d4
Mint <mint29006@gmail.com>
2021-06-18 16:23:09 +0700

    ADD method getSpeciesByID() in species controller

--- src/controller/species.js
+++ src/controller/species.js
@@ -4,16 +4,39 @@ const species = {}
 
 species.getAllSpecies=async()=>{
     let ret = {}
-        ret.message = "Cannot get species"
+        ret.message = "Cannot get data!!"
     try {
-        const result = await pool.query(`SELECT * FROM species`);
+        const ret = await pool.query(`SELECT * FROM species`);
         ret.message = "Sussess :)"
-        console.log(result);
         console.log(ret.message);
-        return result;
+        return ret.rows;
     } catch (err) {
+        ret.message = "Error"
         console.error(err.message);
     }
+
+    return ret.message;
+}
+
+species.getSpeciesByID = async(id) =>{
+    let ret = {}
+    ret.message = "Can't get data"
+
+try {
+    const ret = await pool.query("SELECT * FROM species WHERE species_id = $1", [id]);
+    if(ret.rows.length!=0){
+        ret.message ="Sussess :)"
+        console.log(ret.rows);
+        console.log(ret.message);
+        return ret.rows;
+    } else {
+        ret.message =("Don't have species ID " + id);
+        return ret.message;
+    }
+} catch (err) {
+    console.error(err.message);
+}
+return ret.message;
 }
 
 module.exports = species;
