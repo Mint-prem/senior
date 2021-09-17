57cc801df59ad22c2be579cbd64c922c81dd8cef
Mint <mint29006@gmail.com>
2021-09-18 02:07:30 +0700

    ADD logic in user_request method  in controller

--- src/controller/user_request.js
+++ src/controller/user_request.js
@@ -2,40 +2,6 @@ const pool = require(`../database/pool`);
 
 const request = {}
 
-request.getAllRequest = async()=>{
-    let ret = {}
-        ret.message = "Cannot get data!!"
-    try {
-        const ret = await pool.query(`SELECT * FROM user_request`);
-        ret.message = "Sussess :)"
-        console.log(ret.message);
-        return ret.rows;
-    } catch (err) {
-        console.error(err.message);
-    }
-}
-
-request.getRequestByFarmID = async(id) =>{
-    let ret = {}
-    ret.message = "Can't get data"
-
-    try {
-        const ret = await pool.query("SELECT * FROM user_request WHERE farm_id = $1", [id]);
-        if(ret.rows.length!=0){
-            ret.message ="Sussess :)"
-            console.log(ret.rows);
-            console.log(ret.message);
-            return ret.rows;
-        } else {
-            ret.message =("Don't have farm ID " + id);
-            return ret.message;
-        }
-    } catch (err) {
-        console.error(err.message);
-    }
-    return ret.message;
-}
-
 request.getRequestByUserID = async(id) =>{
     let ret = {}
     ret.message = "Can't get data"
@@ -48,7 +14,7 @@ request.getRequestByUserID = async(id) =>{
             console.log(ret.message);
             return ret.rows;
         } else {
-            ret.message =("Don't have user ID " + id);
+            ret.message =("Don't have any request by user ID " + id);
             return ret.message;
         }
     } catch (err) {
@@ -62,11 +28,19 @@ request.addNewRequest = async(json) =>{
         ret.message = "Cannot create new request"
 
     try {
-        const ret = await pool.query(`INSERT INTO user_request (user_id,farm_id) values ($1,$2)`, 
-        [json.user_id, json.farm_id]);
-        ret.message = "Request Created :)"
-        console.log(ret.message);
-        return ret.message;
+        const findRequest = await pool.query(`SELECT * FROM user_request WHERE user_id = $1`, [json.user_id])
+        if(findRequest.rows.length > 0){
+            ret.message = "Can't create new request because you already request another farm"
+            return ret.message;
+
+        }else{
+            const ret = await pool.query(`INSERT INTO user_request (user_id,farm_id) values ($1,$2)`, 
+            [json.user_id, json.farm_id]);
+            ret.message = "Request Created :)"
+            console.log(ret.message);
+            return ret.message;
+        }
+        
     } catch (err) {
         ret.message = "Error"
         console.error(err.message);
@@ -76,11 +50,11 @@ request.addNewRequest = async(json) =>{
 
 request.cancelRequestByUserID = async(id) => {
     let ret = {}
-        ret.message = "Cannot cancel"
+        ret.message = "Method Error"
         const findByID = await pool.query(`SELECT * FROM user_request WHERE user_id = ` + id)
 
         if(findByID.rows.length==0||null) {
-            ret.message = "Don't have user ID " + id;
+            ret.message = "Don't have any request by user ID " + id;
             return ret.message;
         } else {
             try {
