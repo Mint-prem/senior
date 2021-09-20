bf319906423aa19e461e6af45ce748899358a4b7
Mint <mint29006@gmail.com>
2021-09-20 20:33:06 +0700

    UPDATE worker DB V.3 and logic (Not Done)

--- src/controller/worker.js
+++ src/controller/worker.js
@@ -1,107 +1,283 @@
 const pool = require(`../database/pool`);
 
-const worker = {}
-
-worker.getAllWorker = async()=>{
-    let ret = {}
-        ret.message = "Cannot get data"
+exports.getAllWorker = async (req, res) => {
 
     try {
-        const ret = await pool.query(`SELECT * FROM worker`);
-        ret.message = "Sussess :)"
-        console.log(ret.message);
-        return ret.rows;
+        message = "Method Error"
+
+        const getAllWorker = await pool.query(`SELECT * FROM workers`);
+        message = "Sussess :)"
+        console.log(message);
+        return res.status(200).send({ data: { count: getAllWorker.rowCount, rows: getAllWorker.rows } })
     } catch (err) {
-        ret.message = "Error"
+        message = "Error"
         console.error(err.message);
     }
-    return ret.message;
+    return res.status(500).send({ data: { message: message } });
 }
 
-worker.getWorkerByID = async(id) =>{
-    let ret = {}
-        ret.message = "Cannot get data"
+exports.getWorkerByID = async (req, res) => {
 
     try {
-        const ret = await pool.query("SELECT * FROM worker WHERE farm_id = $1", [id]);
-        if(ret.rows.length!=0){
-            ret.message ="Sussess :)"
-            console.log(ret.message);
-            return ret.rows;
+        let worker_id = req.body.worker_id;
+        message = "Method Error"
+
+        if (worker_id.length == 0 || null) {
+            message = "Please Fill Worker ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        }
+
+        const getWorkerByID = await pool.query(`SELECT * FROM workers WHERE worker_id = $1`, [worker_id])
+        if (getWorkerByID.rows.length != 0) {
+            message = "Sussess :)"
+            console.log(message);
+            return res.status(200).send({ data: { rows: getWorkerByID.rows } })
         } else {
-            ret.message =("Don't have worker ID " + id);
-            return ret.message;
+            message = ("Don't have worker ID " + worker_id);
+            return res.status(500).send({ data: { message: message } });
         }
     } catch (err) {
-        ret.message = "Error";
+        message = "Error";
         console.error(err.message);
     }
-    return ret.message;
+    return res.status(500).send({ data: { message: message } });
 }
 
-worker.addNewWorker = async(json) =>{
-    let ret = {}
-        ret.message = "Cannot create new worker"
+exports.getWorkerByUserID = async (req, res) => {
 
     try {
-        const ret = await pool.query(`INSERT INTO worker (role_id, farm_id, user_id, date_startwork, date_endwork) values ($1,$2,$3,$4,$5)`, 
-        [json.role_id, json.farm_id, json.user_id, json.date_startwork, json.date_endwork]);
-        ret.message = "Worker Add :)"
-        console.log(ret.message);
-        return ret.message;
+        let user_id = req.body.user_id;
+        message = "Method Error"
+
+        if (user_id.length == 0 || null) {
+            message = "Please Fill User ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        }
+
+        const getWorkerByUserID = await pool.query(`SELECT * FROM workers WHERE user_id = $1`, [user_id])
+        if (getWorkerByUserID.rows.length != 0) {
+            message = "Sussess :)"
+            console.log(message);
+            return res.status(200).send({ data: { rows: getWorkerByUserID.rows } })
+        } else {
+            message = ("Don't have worker that has a user ID " + user_id);
+            return res.status(500).send({ data: { message: message } });
+        }
     } catch (err) {
-        ret.message = "Error"
+        message = "Error";
         console.error(err.message);
     }
-    return ret.message;
+    return res.status(500).send({ data: { message: message } });
 }
 
-worker.updateWorkerByID = async(id,json) => {
-     let ret = {}
-        ret.message = "Cannot update worker"
-        const findByID = await pool.query(`SELECT * FROM worker WHERE worker_id = ` + id)
+exports.getWorkerByFarm = async (req, res) => {
 
-        if(findByID.rows.length==0||null) {
-            ret.message = "Don't have worker ID " + id;
-            return ret.message;
+    try {
+        let farm_id = req.body.farm_id;
+        message = "Method Error"
+
+        if (farm_id.length == 0 || null) {
+            message = "Please Fill Farm ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
         } else {
-            try {
-                const ret = await pool.query(`UPDATE worker SET role_id = $1, farm_id = $2, user_id = $3, date_startwork = $4, date_endwork = $5 WHERE cow_id = $6`, 
-                [json.role_id, json.farm_id, json.user_id, json.date_startwork, json.date_endwork, id]);
-                ret.message = "Worker Updated :)"
-                console.log(ret.message);
-                return ret.message;
-            } catch (err) {
-                ret.message = "Error"
-                console.error(err.message);
+            const checkFarmID = await pool.query(`SELECT * FROM farms WHERE farm_id = $1`, [farm_id])
+            if (checkFarmID.rows.length == 0 || null) {
+                message = "Don't have Farm ID: " + farm_id
+                console.log(message)
+                return res.status(500).send({ data: { message: message } })
             }
         }
 
-        return ret.message;
+        const getWorkerByFarmID = await pool.query(`SELECT * FROM workers WHERE farm_id = $1`, [farm_id])
+
+        if (getWorkerByFarmID.rows.length != 0) {
+            message = "Sussess :)"
+            console.log(message);
+            return res.status(200).send({ data: { counts: getWorkerByFarmID.rowCount, rows: getWorkerByFarmID.rows } })
+        } else {
+            message = ("Don't have worker in Farm ID " + farm_id);
+            return res.status(500).send({ data: { message: message } });
+        }
+    } catch (err) {
+        message = "Error";
+        console.error(err.message);
+    }
+    return res.status(500).send({ data: { message: message } });
 }
 
-worker.deleteWorkerByUserID = async(id,json) => {
-    let ret = {}
-        ret.message = "Cannot Delete worker"
-        const findByID = await pool.query(`SELECT * FROM worker WHERE user_id = ` + json.user_id)
+exports.addNewWorker = async (req, res) => {
 
-        if(findByID.rows.length==0||null) {
-            ret.message = "Don't have worker ID " + json.user_id;
-            return ret.message;
-        } else {
-            try {
-                const ret = await pool.query(`DELETE FROM worker WHERE farm_id = $1 AND user_id = $2`, [id, json.user_id]);
-                ret.message = "Worker Deleted :)"
-                console.log(ret.message);
-                return ret.message;
-            } catch (err) {
-                ret.message = "Error"
-                console.error(err.message);
-            }
+    try {
+        const user_id = req.body.user_id
+        const farm_id = req.body.farm_id
+
+        message = "Method Error"
+        const date = new Date();
+        var startwork = date.toISOString();
+
+        if (user_id.length == 0 || null) {
+            message = "Please Fill User ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        } else if (farm_id.length == 0 || null) {
+            message = "Please Fill Farm ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
         }
 
-        return ret.message;
+        const checkHaveUser = await pool.query(`SELECT * FROM users WHERE user_id = $1`, [user_id])
+        const checkHaveFarm = await pool.query(`SELECT * FROM farms WHERE farm_id = $1`, [farm_id])
 
+
+        if (checkHaveUser.rows.length == 0 || null) {
+            message = "Don't have user ID: " + user_id
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        } else if (checkHaveFarm.rows.length == 0 || null) {
+            message = "Don't have farm ID: " + farm_id
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        }
+
+        const addWorker = await pool.query(`INSERT INTO workers (role_id, farm_id, user_id, startwork) values ($1,$2,$3,$4)`,
+            [2, farm_id, user_id, startwork]);
+
+        const checkUser = await pool.query(`SELECT * FROM workers WHERE user_id = $1`, [user_id])
+
+        if (checkUser.rows.length != 0) {
+            message = "Worker Add :)"
+            console.log(message);
+            return res.status(200).send({ data: { counts: checkUser.rowCount, rows: checkUser.rows } })
+        } else {
+            message = "Can't Add Worker"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        }
+
+    } catch (err) {
+        message = "Error"
+        console.error(err.message);
+    }
+    return res.status(500).send({ data: { message: message } });
 }
 
-module.exports = worker;
+exports.updateWorkerByID = async (req, res) => {
+
+    try {
+        //logic ยังไม่ครอบคลุม
+        const worker_id = req.body.worker_id
+        const user_id = req.body.user_id
+        const farm_id = req.body.farm_id
+        const startwork = req.body.startwork
+        message = "Method Error"
+
+        if (worker_id.length == 0 || null) {
+            message = "Please Fill Worker ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        } else if (farm_id.length == 0 || null) {
+            message = "Please Fill Farm ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        } else if (startwork.length == 0 || null) {
+            message = "Please Fill Startwork"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        } else if (user_id.length == 0 || null) {
+            message = "Please Fill User ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        }
+
+        const findWorker = await pool.query(`SELECT * FROM workers WHERE worker_id = ` + worker_id)
+
+        if (findWorker.rows.length == 0 || null) {
+            message = "Don't have worker ID " + id;
+            return res.status(500).send({ data: { message: message } })
+        }
+        const updateWorker = await pool.query(`UPDATE worker SET role_id = $1, farm_id = $2, user_id = $3, startwork = $4 WHERE worker_id = $5`,
+            [role_id, farm_id, user_id, startwork, worker_id]);
+
+        const newUpdate = await pool.query(`SELECT * FROM workers WHERE worker_id = ` + worker_id)
+
+        message = "Worker Updated :)"
+        console.log(message);
+
+        return res.status(200).send({ data: { message: message, rows: newUpdate.rows } })
+
+    } catch (err) {
+        message = "Error"
+        console.error(err.message);
+    }
+    return res.status(500).send({ data: { message: message } })
+}
+
+exports.deleteWorkerByUserID = async (req, res) => {
+
+    try {
+        //logic ยังไม่ครอบคลุม
+        const farm_id = req.body.farm_id
+        const user_id = req.body.user_id
+        message = "Method Error"
+
+        if (farm_id.length == 0 || null) {
+            message = "Please Fill Farm ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        } else if (user_id.length == 0 || null) {
+            message = "Please Fill User ID"
+            console.log(message)
+            return res.status(500).send({ data: { message: message } })
+        }
+
+        const findByID = await pool.query(`SELECT * FROM workers WHERE user_id = $1 AND farm_id = $2` ,[user_id, farm_id])
+
+        if (findByID.rows.length == 0 || null) {
+            message = "Don't have user ID " + user_id + " in Farm ID: " + farm_id;
+            return res.status(500).send({ data: { message: message } })
+        }
+        const deleteWorker = await pool.query(`DELETE FROM worker WHERE farm_id = $1 AND user_id = $2`, [farm_id, user_id]);
+        message = "Worker Deleted :)"
+        console.log(message);
+        return res.status(200).send({ data: { message: message } })
+
+    } catch (err) {
+        message = "Error"
+        console.error(err.message);
+    }
+
+    return res.status(500).send({ data: { message: message } })
+
+    // try {
+    //     const user_id = req.body.user_id
+    //     message = "Method Error"
+
+    //     if (user_id.length == 0 || null) {
+    //         message = "Please Fill User ID"
+    //         console.log(message)
+    //         res.status(500).send({ data: { message: message } })
+    //     } else {
+    //         const checkUser = await pool.query(`SELECT * FROM users WHERE user_id = $1`, [user_id])
+
+    //         if(checkUser.rows.length==0||null){
+    //             message = "Don't have User ID: " + user_id
+    //             console.log(message)
+    //             res.status(500).send({ data: { message: message } })
+    //         }  else {
+    //             const checkWorker = await pool.query("SELECT * FROM workers WHERE user_id = $1", [user_id]);
+
+    //             if(checkWorker.rows.length != 0){
+
+
+    //             } else{
+    //                 message = "User don't have Farm yet"
+    //                 console.log(message)
+    //                 res.status(500).send({ data: { message: message } })
+    //             }
+    //         }
+    //     }
+
+}
