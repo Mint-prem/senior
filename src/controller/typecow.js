bf5c1a97eb50d5ea1d6194dcacfe7500172f61bf
Mint <mint29006@gmail.com>
2021-09-21 16:56:08 +0700

    UPDATE return in type cow

--- src/controller/typecow.js
+++ src/controller/typecow.js
@@ -7,13 +7,13 @@ exports.getAllTypecow = async (req, res) => {
         const getAllType = await pool.query(`SELECT * FROM typecow`);
         message = "Sussess :)"
         console.log(message);
-        res.status(200).send({ data: { typecow: getAllType.rows } })
+        return res.status(200).send({ data: { typecow: getAllType.rows } })
     } catch (err) {
         message = "Error"
         console.error(err)
     }
 
-    res.status(500).send({data : { message : message } });
+    return res.status(500).send({data : { message : message } });
 }
 
 exports.getTypecowByID = async(req, res) => {
@@ -24,7 +24,7 @@ exports.getTypecowByID = async(req, res) => {
     if(id.length==0||null){
         message = "Please Fill Typecow ID"
         console.log(message)
-        res.status(500).send({data : { message : message } })
+        return res.status(500).send({data : { message : message } })
     }
 
     try {
@@ -32,14 +32,14 @@ exports.getTypecowByID = async(req, res) => {
         if(getTypeByID.rows.length!=0){
             message = "Sussess :)"
             console.log(message);
-            res.status(200).send({data : {typecow : getTypeByID.rows}})
+            return res.status(200).send({data : {typecow : getTypeByID.rows}})
         } else {
             message = "Don't have typecow ID : " + id;
-            res.status(500).send({data : { message : message } })
+            return res.status(500).send({data : { message : message } })
         }
     } catch (err) {
         message = "Error"
         console.error(err.message);
     }
-    res.status(500).send({data : { message : message } });
+    return res.status(500).send({data : { message : message } });
 }
